ac58e87b158d2f39ec46ee436245c04b3db5a112
Nishikantaray <nishikantaray1@gmail.com>
2022-07-07 14:31:55 +0530

    added footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Footer from './layouts/Footer';
 import Navbar from './layouts/Navbar';
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
-      
+      <Footer/>
     </div>
   );
 }
